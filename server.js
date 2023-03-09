863bedccd3f1ec0812e93ba378ecb0b289e9ef64
Bri Dray <stanggurl02@gmail.com>
2023-03-08 18:39:13 -0800

    fixed paths, started to add delete function. started on my README.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const note = require("./Develop/db/db.json")
+const notes = require("./Develop/db/db.json")
 const path = require("path");
 const uuid = require("uuid");
 
@@ -10,26 +10,32 @@ const PORT = 3001;
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('./Develop/public'));
 
-//saves note and adds to db.json
-app.get("/api/note", (req, res) => {
+//deletes notes and adds to db.json
+app.delete("/api/notes/:id", (req, res) => {
+  // res.sendFile(path.join(__dirname, "./Develop/db/db.json"))
+  console.log(req.params.id)
+});
+
+//saves notes and adds to db.json
+app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./Develop/db/db.json"))
 });
 
-// function to add new note
-app.post("/api/note", (req,res) => {
-    const note = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
-    const newEntry = req.body;
-    newEntry.id = uuid.v4();
-    note.push(newEntry);
-    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(note));
-    res.json(note);
+// function to add new notes
+app.post("/api/notes", (req,res) => {
+    const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
+    res.json(notes);
 })
 
-// calling note.html
-app.get('/note', (req, res) => {
-  res.sendFile(path.join(__dirname,'./Develop/public/note.html' ));
+// calling notes.html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'./Develop/public/notes.html' ));
 });
 
 //calling index.html
@@ -41,3 +47,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
+
