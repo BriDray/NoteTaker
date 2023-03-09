a66beaa64ca728f433134f9cdc82277a7b3010b1
Bri Dray <stanggurl02@gmail.com>
2023-03-08 21:45:30 -0800

    fixed a few typos.

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/notes', (req, res) => {
 });
 
 //calling index.html
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
